d4a9df62a9b7bd4c2ac716463802f6a4a381a1bf
Marcus Little <mcolelittle@gmail.com>
2019-01-24 15:18:37 -0700

    added postgres configurations for dev and prod environments

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
+const logger = require('morgan');
+const express = require("express");
+const app = express();
 
 module.exports = {
   init(){
-
+    app.use(logger('dev'));
   }
 };
