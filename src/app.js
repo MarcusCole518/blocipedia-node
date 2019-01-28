968275161b57da6a61493d7c437450e7458b9d85
Marcus Little <mcolelittle@gmail.com>
2019-01-28 16:45:04 -0700

    working on user signup

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
 routeConfig.init(app);
-appConfig.init();
+appConfig.init(app, express);
 
 
 module.exports = app;
