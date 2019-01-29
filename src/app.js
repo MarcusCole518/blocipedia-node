ecbca522f0fa76e6dbca54b9ec4df2d6b34ab408
Marcus Little <mcolelittle@gmail.com>
2019-01-29 10:45:09 -0700

    fixing validation/css issues

--- src/app.js
+++ src/app.js
@@ -7,7 +7,5 @@ const routeConfig = require("./config/route-config.js");
 routeConfig.init(app);
 appConfig.init(app, express);
 
-app.use("/css", express.static(__dirname + "/css"))
-
 
 module.exports = app;
