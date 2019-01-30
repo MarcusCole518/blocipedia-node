616dc0cc7991dd7f349f13f2560f27be8f55a83d
Marcus Little <mcolelittle@gmail.com>
2019-01-29 19:52:00 -0700

    implemented user signup

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const app = express();
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
-routeConfig.init(app);
-appConfig.init(app, express);
+app.use("/css",express.static(__dirname + "/css"));
 
-app.use("/css", express.static(__dirname + "/css"))
+appConfig.init(app, express);
+routeConfig.init(app);
 
 
 module.exports = app;
