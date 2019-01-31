85a54fbe2a76262a180a11cb9f84d877252a54a4
Marcus Little <mcolelittle@gmail.com>
2019-01-31 13:03:04 -0700

    created query interface, implemented methods for rendering wikis

---
+++ spec/integration/wikis_spec.js
@@ -0,0 +1,51 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/wikis/";
+const sequelize = require("../../src/db/models/index").sequelize;
+const Wiki = require("../../src/db/models").Wiki;
+const User = require("../../src/db/models").User;
+
+describe('routes : wikis', () => {
+
+    beforeEach((done) => {
+        this.wiki;
+        this.user;
+        sequelize.sync({force: true}).then((res) => {
+            User.create({
+                email: "user@example.com",
+                password: "1234567890"
+            })
+            .then((user) => {
+                this.user = user;
+
+                Wiki.create({
+                    title: "My first wiki",
+                    body: "this is an example wiki",
+                    private: false,
+                    userId: this.user.id
+                })
+                .then((wiki) => {
+                    this.wiki = wiki;
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            })
+        });
+    });
+
+    describe("GET /wikis", () => {
+
+        it("should return a status code 200 and all wikis", (done) => {
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(err).toBeNull();
+                expect(body).toContain("Wikis");
+                expect("My first wiki");
+                done();
+            });
+        });
+    });
+});
---
+++ src/db/queries.wikis.js
@@ -0,0 +1,15 @@
+const Wiki = require("./models").Wiki;
+const User = require("./models").User;
+
+module.exports = {
+
+    getAllWikis(callback){
+        return Wiki.findAll()
+        .then((wikis) => {
+            callback(null, wikis);
+        })
+        .catch((err) => {
+            callback(err);
+        })
+    }
+}
